b1b34978ab44cbc2530adeb16ff13371882919b6
Jasper <jappe_1999@outlook.com>
2017-01-11 18:00:15 +0100

    Stash

--- js/get_file.js
+++ js/get_file.js
@@ -1,16 +1,38 @@
-var file = function(url) {
+var file = function(method, url) {
+  this.method = method;
   this.url = url;
 };
 
+//Get parse_page
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
 
 // Get page
 file.prototype.get_page = function () {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     return document.getElementByClassName("avt_table").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", this.url, true);
-  xhttp.send();
-};
+  xhr = createCORSRequest(this.method, this.url);
+  if (!xhr) {
+    throw new Error('CORS not supported');
+  }
+  return xhr;
+}
