a6a711b4baccb4f7ec87ff8108c42ed422563bd5
midyro <Mitch@MacBook-Air-van-Mitch.local>
2017-01-11 16:53:16 +0100

    added more lines

--- text.php
+++ text.php
@@ -1,9 +1,12 @@
 <div class="treinOptijd">
   <h3>Onmogelijk je Fucking trein is op tijd.</h3>
+  <h3>Verrassing je trein is op tijd, voor het eerst</h3>
 </div>
 <div class="treinTelaat">
   <h3>HAHAHHAHAA je trein is zoals gewoonlijk te laat HAHAHAH KARMA</h3>
+  <h3>Was te verwachten, het is en blijft NS</h3>
 </div>
 <div class="treinTevroeg">
   <h3>Wat is de NS ziek? het kan niet dat het op tijd is, controleer nog even of hij niet te laat komt</h3>
+  <h3>Wat een mazzel, je behoort nu bij de 1% van Nederland waarbij de trein te vroeg kwam</h3>
 </div>
