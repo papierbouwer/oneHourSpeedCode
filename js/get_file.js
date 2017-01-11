74c1bed0a837a4d9d2b34167d579e3001363bdd2
Jasper <jappe_1999@outlook.com>
2017-01-11 17:35:33 +0100

    Stash

--- js/get_file.js
+++ js/get_file.js
@@ -1,14 +1,14 @@
 var file = function(url) {
   this.url = url;
-}
+};
 
 
+// Get page
 file.prototype.get_page = function () {
-  // Get page
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementByClassName("").innerHTML = this.responseText;
+     return document.getElementByClassName("avt_table").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", this.url, true);
