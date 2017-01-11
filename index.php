661407d87cd7a3f6003b2849f49a4d7724a01338
Erik Baalbergen <erikbaalbergen@gmail.com>
2017-01-11 16:29:58 +0100

    link gevonden voor tijden.

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 
 <?php
 ?>
+<!--
 
+http://www.ns.nl/actuele-vertrektijden/avt?station=alm
+
+-->
 
 <html lang="nl_NL">
     <head>
         <title>Je fucking treintijden BITCH!</title>
     </head>
     <body>
-        
+
     </body>
 </html>
