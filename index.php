3b16c7dbbc0357c57d15993e62f7d546d60c728b
Jasper <jappe_1999@outlook.com>
2017-01-11 16:51:35 +0100

    Added css

--- index.php
+++ index.php
@@ -11,13 +11,16 @@ $doc = $doc->getElementById("id3");
 ?>
 <!--
 
-http://www.ns.nl/actuele-vertrektijden/avt?station=alm
+url: http://www.ns.nl/actuele-vertrektijden/avt?station=alm
 
 -->
 
 <html lang="nl_NL">
     <head>
         <title>Je fucking treintijden BITCH!</title>
+        <meta charset="utf-8" />
+        <link style="text/css" rel="stylesheet" href="css/style.css" />
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
     </head>
     <body>
         <?php echo $doc->saveHTML();?>
